647b0cb15858940326f654d1b5d4f506c014c139
Mohamed Abduljawad <salintofficial@hotmail.com>
2022-02-19 13:16:46 +0200

    Added main page

---
+++ components/header.php
@@ -0,0 +1,3 @@
+<header>
+	<h1>Anonymous Honesty</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php
+
+		require "components/head.php";
+
+	?>
+	<body>
+			
+	<?php
+
+		require "components/header.php";
+
+	?>
+
+	</body>
+</html>
